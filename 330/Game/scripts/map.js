e0a7f59774fdbc62d75804f47a0320900387c50f
Ryan Bell <rjb1928@rit.edu>
2017-05-18 00:03:06 +0100

    added noise, updated map

--- 330/Game/scripts/map.js
+++ 330/Game/scripts/map.js
@@ -1,32 +1,39 @@
 function createMap(){
-    let map = {};
-		map[0] = content['grass2'];
-		map[1] = content['dirt2'];
-    map.tiles = [
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1],
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-    ];
-    map.frames = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]];
-    map.render = function(context){
-        //TODO: temp
-        let tilesize = 64;
-        map.tiles.forEach(function(y, yi){
-            y.forEach(function(x, xi){
-                map.frames[xi][yi] = map.frames[xi][yi] || getRandomInt(0, map[x].frame_count - 1);
-                map[x].frame = map.frames[xi][yi];
-                map[x].render({context: context, x: xi * tilesize, y: yi * tilesize});
-            });
-        });
-    }
+	const mapx = 20;
+	const mapy = 20;
+	const tilesize = 64;
 
-    return map;
+	let map = {};
+	map.content = [];
+	map.content[0] = content['grass2'];
+	map.content[1] = content['dirt2'];
+
+	map.tiles = [];
+
+	for (var x = 0; x < mapx; x++) {
+		map.tiles[x] = map.tiles[x] || [];
+		for (var y = 0; y < mapy; y++) {
+			map.tiles[x][y] = map.tiles[x][y] || [];
+			// All noise functions return values in the range of -1 to 1.
+			//var value = noise.simplex2(x / 100, y / 100);
+			var value = noise.simplex2(x, y);
+			value = Math.abs(value) * (map.content.length - 1);
+			value = Math.round(value);
+
+			map.tiles[x][y].value = value;
+			map.tiles[x][y].frame = getRandomInt(0, map.content[value].frame_count - 1);
+		}
+	}
+
+	map.render = function(context){
+		map.tiles.forEach(function(y, yi){
+			y.forEach(function(x, xi){
+				let tile = x;
+				map.content[tile.value].frame = tile.frame;
+				map.content[tile.value].render({context: context, x: xi * tilesize, y: yi * tilesize});
+			});
+		});
+	}
+
+	return map;
 }
